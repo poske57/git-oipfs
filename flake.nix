694d39085242325a85f10917c5fe04dc58d625d9
poske57 <poske+github@ubukha.com>
2026-09-14 00:08:51 +0900

    build: go development environment

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Go development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs systems (system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+            };
+          }
+        );
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            gotools
+            golangci-lint
+          ];
+
+          shellHook = ''
+            echo "Go version: $(go version)"
+          '';
+        };
+      });
+    };
+}
+
