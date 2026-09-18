1728b4fdf848a287b49246e74d5ba5cb7a27b684
poske57 <poske+github@ubukha.com>
2026-09-18 19:23:14 +0900

    feat: get HEAD hash

--- app/main.go
+++ app/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	git "github.com/go-git/go-git/v6"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+    hash, err := headHash()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(hash)
+}
+
+func headHash() (string, error) {
+	r, err := git.PlainOpen("/home/poske/Projects/git-oipfs/")
+	if err != nil {
+		return "", err
+	}
+
+	ref, err := r.Head()
+	if err != nil {
+		return "", err
+	}
+
+	return ref.Hash().String(), nil
 }
